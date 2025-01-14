5f29bdb90fff874febd102bb435fa68edd8a5d25
Encarna <encars28@gmail.com>
2025-01-14 23:48:04 +0100

    file structure and change models

---
+++ backend/app/app.py
@@ -0,0 +1,2 @@
+from sqlmodel import Session
+from app.core.database import create_db_and_tables, engine
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,4 @@
+from pydantic import EmailStr
 from sqlmodel import SQLModel, Field, Relationship
 from typing import List, Optional
 
@@ -8,13 +9,16 @@ class Author(SQLModel, table=True):
     birth_year: Optional[int]
 
     poems: List["Poem"] = Relationship(back_populates="author")
+    
+    # https://stackoverflow.com/questions/70759112/one-to-one-relationships-with-sqlmodel
+    user: Optional["User"] = Relationship(sa_relationship_kwargs={'uselist': False}, back_populates="author_info")
+
 
 class PoemCollectionLink(SQLModel, table=True):
     poem_id: Optional[int] = Field(default=None, foreign_key="poem.id", primary_key=True)
     collection_id: Optional[int] = Field(default=None, foreign_key="collection.id", primary_key=True)
-    
+
 class Poem(SQLModel):
-    id: Optional[int] = Field(default=None, primary_key=True)
     title: str
     content: str
 
@@ -26,31 +30,38 @@ class Poem(SQLModel):
     collections: List["Collection"] = Relationship(back_populates="poems", link_model=PoemCollectionLink)
 
 class OriginalPoem(Poem, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     versions: List["PoemVersion"] = Relationship(back_populates="original")
     translations: List["PoemTranslation"] = Relationship(back_populates="original")
 
 class PoemVersion(Poem, table=True):
-    original_id: int = Field(foreign_key="poem.id")
+    id: Optional[int] = Field(default=None, primary_key=True)
+    original_id: int = Field(foreign_key="originalpoem.id")
     original: Poem = Relationship(back_populates="versions")
 
 class PoemTranslation(Poem, table=True):
-    original_id: int = Field(foreign_key="poem.id")
+    id: Optional[int] = Field(default=None, primary_key=True)
+    original_id: int = Field(foreign_key="originalpoem.id")
     original: Poem = Relationship(back_populates="translations")
 
 class Collection(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
+    title: str
 
     publication_year: Optional[int]
 
     poems: List[Poem] = Relationship(back_populates="collection", link_model=PoemCollectionLink)
 
+class UserBase(SQLModel):
+    username: str = Field(unique=True, max_length=255)
+    email: EmailStr = Field(unique=True, max_length=255)
+    full_name: Optional[str] = Field(max_length=255)
+
+    is_admin: bool = False
+
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    username: str = Field(unique=True)
-    password: str
+    hashed_password: str
 
-    # if this were to become a many-to-many relationship, (several roles
-    # per user), we would need to create a new table for this relationship
-    # For now the only roles are "user" and "admin"
-    role: str = Field(default="user")	
+    author_id: Optional[int] = Field(foreign_key="author.id")
+    author_info: Optional[Author] = Relationship(back_populates="user")
