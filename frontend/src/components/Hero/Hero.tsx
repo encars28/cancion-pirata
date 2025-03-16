2150469783ade21227f8554d07da7e3d9b79dd12
encar <encars28@gmail.com>
2025-03-16 23:15:19 +0100

    hero prettier

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -1,7 +1,9 @@
-import { Button, Center, Overlay, Text, Title, Flex, Stack, Space } from '@mantine/core';
+import { Button, Center, Overlay, Text, Title, Flex, Stack } from '@mantine/core';
 import classes from './Hero.module.css';
+import { useNavigate } from 'react-router';
 
 export function Hero() {
+  const navigate = useNavigate();
   return (
     <div className={classes.wrapper}>
       <Overlay color="#000" opacity={1} zIndex={1} />
@@ -15,8 +17,14 @@ export function Hero() {
           </Title>
         </Center>
         <Center>
-          <Text ta="center" className={classes.description}>
+          {/* <Text ta="center" className={classes.description}>
             Poemas! Navega nuestro gran y amplio catálogo de poemas y autores.
+          </Text> */}
+          <Text ta="center" className={classes.description}>
+            “If you know the enemy and know yourself, you need not fear the result of a hundred battles.<br />
+            If you know yourself but not the enemy, for every victory gained you will also suffer a defeat. <br />
+            If you know neither the enemy nor yourself, you will succumb in every battle.”
+            <Text ta="right" pt="md" size="md">-- Sun Tzu, The Art of War</Text>
           </Text>
         </Center>
         <Flex
@@ -26,10 +34,17 @@ export function Hero() {
           gap="xl"
           mt="xl"
         >
-          <Button className={classes.control} variant="white">
+          <Button
+            className={classes.control}
+            variant="white"
+            onClick={() => navigate("/poems")}
+          >
             Poemas
           </Button>
-          <Button className={classes.control}>
+          <Button
+            className={classes.control}
+            onClick={() => navigate("/authors")}
+          >
             Autores
           </Button>
         </Flex>
--- frontend/src/pages/mainpage.tsx
+++ frontend/src/pages/mainpage.tsx
@@ -1,56 +1,10 @@
-import {
-  Text,
-  Button,
-  Group,
-  Space,
-  Stack,
-} from '@mantine/core';
-
 import { Shell } from '../components/Shell/Shell';
-import { useNavigate } from 'react-router';
 import { Hero } from '../components/Hero/Hero';
 
-function ShowQuote() {
-  return (
-    <Stack align='center' mt="xl">
-      <Space h="xl" />
-      <Text pt="xl">
-        “If you know the enemy and know yourself, you need not fear the result of a hundred battles.<br />
-        If you know yourself but not the enemy, for every victory gained you will also suffer a defeat. <br />
-        If you know neither the enemy nor yourself, you will succumb in every battle.”
-      </Text>
-      <Text pl={{ base: 250, xs: 400, sm: 450, md: 500, lg: 500 }}>-- Sun Tzu, The Art of War</Text>
-    </Stack>
-  )
-}
-
 export function MainPage() {
-  const navigate = useNavigate();
   return (
     <Shell>
-      {/* <Stack justify='center' gap="xl">
-        <ShowQuote />
-        <Space h="lg" />
-        <Group justify='center' gap="xl">
-          <Button
-            variant='default'
-            w={200}
-            onClick={() => navigate('/poems')}
-          >
-            Ver Poemas
-          </Button>
-          <Button
-            variant='default'
-            w={200}
-            onClick={() => navigate('/authors')}
-          >
-            Ver Autores
-          </Button>
-        </Group>
-      </Stack> */}
       <Hero />
     </Shell>
   )
 }
-
-
