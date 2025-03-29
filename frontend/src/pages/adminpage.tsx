119c2906158575b7f3938386c63694df5e8a2c04
encar <encars28@gmail.com>
2025-03-29 18:51:55 +0100

    update admin page with add, delete and edit user

--- frontend/src/pages/adminpage.tsx
+++ frontend/src/pages/adminpage.tsx
@@ -4,7 +4,10 @@ import { callService, handleError } from "../utils";
 import { UserPublic, usersReadUsers } from "../client";
 import { Loading } from "../components/Loading";
 import { TableSort } from "../components/Tables/TableSort";
-import { Badge, Center, Container } from "@mantine/core";
+import { Badge, Center, Container, Group } from "@mantine/core";
+import { AddUser } from "../components/User/AddUser";
+import { EditUser } from "../components/User/EditUser";
+import { DeleteUser } from "../components/User/DeleteUser";
 
 export function AdminPage() {
   const { isPending, isError, data, error } = useQuery({
@@ -31,6 +34,7 @@ export function AdminPage() {
     created_at: 'Creación',
     author_id: 'ID autor',
     is_active: 'Estatus',
+    actions: 'Acciones'
   }
 
   const userData = users.map((user) => {
@@ -43,11 +47,15 @@ export function AdminPage() {
       created_at: user.created_at?.toLocaleDateString() ?? '',
       author_id: user.author_id ?? '',
       is_active: user.is_active ? "Activo" : "Inactivo",
+      actions: <Group gap="xs"><EditUser user={user} /><DeleteUser user_id={user.id} /></Group>
     }
   })
 
   return (
     <Shell>
+      <Group justify="flex-end" m="xl" mr={{base: "xl", sm: 100}}>
+        <AddUser />
+      </Group>
       <Center m={60}>
         <TableSort headers={userHeaders} data={userData} />
       </Center>
