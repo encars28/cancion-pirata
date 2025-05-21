c4f11103a0f0f74a191f5b96c0436f05825dad84
encar <encars28@gmail.com>
2025-05-22 01:43:59 +0200

    pagina de poema bonita

--- frontend/src/pages/poempage.tsx
+++ frontend/src/pages/poempage.tsx
@@ -4,11 +4,18 @@ import { PoemPublicWithAllTheInfo } from '../client/types.gen';
 import { Loading } from '../components/Loading';
 import { useNavigate, useParams } from 'react-router';
 import useAuth from '../hooks/useAuth';
-import { ShowPoem } from '../components/Poem/ShowPoem';
-import { Button, Stack, Text, Space, Group } from '@mantine/core';
+import { List, Title, Container, Flex, Stack, Text, Space, Group, ActionIcon, Tooltip, Anchor } from '@mantine/core';
 import usePoem from '../hooks/usePoem';
 import usePoemActions from '../hooks/usePoemActions';
 import { modals } from '@mantine/modals';
+import { TbPencil, TbTrash } from 'react-icons/tb';
+import { Interweave } from 'interweave';
+import { InfoBox } from '../components/InfoBox';
+
+enum PoemType {
+  TRANSLATION = 0,
+  VERSION = 1,
+}
 
 export function PoemPage() {
   const params = useParams();
@@ -46,25 +53,97 @@ export function PoemPage() {
   return (
     <Shell>
       <Space h="xl" />
-      <Stack gap="xs">
-        {((poem.author_ids && currentUser?.author_id && (poem.author_ids.includes(currentUser?.author_id))) || currentUser?.is_superuser) && (
-          <Group justify='flex-end' mt="xl" mr={{ base: "xl", md: 100, lg: 150 }}>
-            <Button
-              variant="outline"
-              onClick={() => navigate(`/poems/edit/${poem.id}`)}
-            >
-              Editar poema
-            </Button>
-            <Button
-              color="red"
-              onClick={deletePoem}
-            >
-              Eliminar poema
-            </Button>
-          </Group>
-        )}
-        <ShowPoem poem={poem} />
-      </Stack>
+      <Flex
+        justify="center"
+        wrap='wrap'
+        mt="xl"
+      >
+        <Flex
+          justify="center"
+          align="center"
+          direction="column"
+          gap="xl"
+          mt="xl"
+          w={{ base: "100%", sm: "60%" }}
+        >
+          <Container fluid>
+              <Title order={1}>{poem.title}</Title>
+            <Title order={3} mt="xs" c="dimmed" fw="lighter">
+              Autor: {poem.author_names?.length == 0 ? "Anónimo" : poem.author_names?.join(", ")}
+            </Title>
+            {((poem.author_ids && currentUser?.author_id && (poem.author_ids.includes(currentUser?.author_id))) || currentUser?.is_superuser) && (
+                <Group justify='center' mt="lg" gap="xs">
+                  <Tooltip label="Editar">
+                    <ActionIcon
+                      variant="outline"
+                      size={35}
+                      onClick={() => navigate(`/poems/edit/${poem.id}`)}
+                    >
+                      <TbPencil size={20} />
+                    </ActionIcon>
+                  </Tooltip>
+                  <Tooltip label="Eliminar">
+                    <ActionIcon
+                      color="red"
+                      size={35}
+                      onClick={deletePoem}
+                    >
+                      <TbTrash size={20} />
+                    </ActionIcon>
+                  </Tooltip>
+                </Group>
+              )}
+          </Container>
+          <Container fluid><Interweave content={poem.content} /></Container>
+          <Space h="xl" />
+        </Flex>
+        <Stack>
+          {
+            (poem.original && poem.type == PoemType.TRANSLATION) &&
+            (
+              <InfoBox>
+                <Text>
+                  Este poema es una traducción.
+                </Text>
+                <Anchor onClick={ () => navigate(`/poems/${poem.original!.id}`)}>
+                  Ver poema original
+                </Anchor>
+              </InfoBox>
+            )
+          }
+          {
+            (poem.original && poem.type == PoemType.VERSION) &&
+            (
+              <InfoBox>
+                <Text>
+                  Este poema es una versión.
+                </Text>
+                <Anchor onClick={ () => navigate(`/poems/${poem.original!.id}`)}>
+                  Ver poema original
+                </Anchor>
+              </InfoBox>
+            )
+          }
+          {
+            (poem.derived_poems && poem.derived_poems.length > 0) && (
+              <InfoBox>
+                <Text>
+                  Este poema tiene derivados.
+                </Text>
+                <List ta="left" withPadding>
+                  {poem.derived_poems.map((derivedPoem) => (
+                    <List.Item key={derivedPoem.id}>
+                      <Anchor onClick={ () => navigate(`/poems/${derivedPoem.id}`)}>
+                        {derivedPoem.title}
+                      </Anchor>
+                    </List.Item>
+                  ))}
+                </List>
+              </InfoBox>
+            )
+          }
+        </Stack>
+      </Flex>
     </Shell>
   )
 }
